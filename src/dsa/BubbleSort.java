1d237e09df8cd5c7013e8658f2b8f42269a3e8de
ludwig-han <ludwighan.dev@gmail.com>
2025-09-05 23:59:11 +0900

    bubble sort

--- src/dsa/BubbleSort.java
+++ src/dsa/BubbleSort.java
@@ -2,6 +2,11 @@ package dsa;
 
 public class BubbleSort {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		int arr[] = { 7, 12, 9, 11, 3 };
+		
+		for (int i=0; i<arr.length - 1;i++) {
+			for (int j=)
+		}
+		
 	}
 }
